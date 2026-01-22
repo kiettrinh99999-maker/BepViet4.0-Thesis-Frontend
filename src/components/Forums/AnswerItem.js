6265e72209269aee122fd046b6ef4b4d4ff38b25
tnnq <0306231146@caothang.edu.vn>
2026-01-22 16:51:37 +0700

    Chuc nang them question va answer

--- src/components/Forums/AnswerItem.js
+++ src/components/Forums/AnswerItem.js
@@ -2,7 +2,56 @@ import { useState } from "react";
 import { useAuth } from "../../contexts/Authen";
 export default function AnswerItem({ answer }) {
     const [showReply, setShowReply] = useState(false);
-    const { renderDate } = useAuth();
+    const [replyContent, setReplyContent] = useState("");
+    const [loading, setLoading] = useState(false);
+    const {user, api, renderDate } = useAuth();
+    async function handleReplySubmit() {
+        if (!user) {
+            alert("Vui lòng đăng nhập để trả lời");
+            return;
+        }
+
+        if (!replyContent.trim()) {
+            alert("Vui lòng nhập nội dung phản hồi");
+            return;
+        }
+
+        setLoading(true);
+
+        try {
+            const res = await fetch(`${api}answers`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                content: replyContent,
+                question_id: answer.question_id,
+                parent_id: answer.id,
+            }),
+            });
+
+            const data = await res.json();
+
+            if (!res.ok) {
+            console.error(data);
+            alert("Gửi phản hồi thất bại");
+            return;
+            }
+
+            setReplyContent("");
+            setShowReply(false);
+
+            //reload trang
+            // window.location.reload();
+
+        } catch (err) {
+            console.error(err);
+            alert("Không thể kết nối server");
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
         <div className="mb-4">
             <div className="d-flex mb-2">
@@ -23,7 +72,7 @@ export default function AnswerItem({ answer }) {
 
                     <p className="mb-2">{answer.content}</p>
 
-                    <button className="btn btn-link p-0 text-decoration-none" onClick={() => setShowReply(!showReply)}>
+                    <button className="comment-action" onClick={() => setShowReply(!showReply)}>
                         <i className="far fa-comment me-1"></i> Trả lời
                     </button>
                 </div>
@@ -32,14 +81,14 @@ export default function AnswerItem({ answer }) {
             {/* Form để answer */}
             {showReply && (
                 <div className="ms-5 mb-3">
-                    <textarea className="form-control mb-2" rows="3"
-                        placeholder="Viết phản hồi..." />
+                    <textarea className="form-control mb-2" rows="3" placeholder="Viết phản hồi..." 
+                        value={replyContent} onChange={(e) => setReplyContent(e.target.value)}/>
                     <div className="d-flex justify-content-end">
-                        <button className="btn btn-outline-secondary btn-sm me-2" onClick={() => setShowReply(false)}>
+                        <button className="btn btn-outline-secondary btn-sm me-2 px-2 py-1" onClick={() => setShowReply(false)} style={{ width: "auto" }}>
                             Hủy
                         </button>
-                        <button className="btn btn-danger btn-sm">
-                            Gửi
+                        <button className="btn btn-danger btn-sm  px-2 py-1" style={{ width: "auto" }}  onClick={handleReplySubmit}>
+                            {loading ? "Đang gửi..." : "Gửi"}
                         </button>
                     </div>
                 </div>
